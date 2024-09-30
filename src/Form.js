e191fe96a1dd4c6b3f97ff76b88a0fdf8b3643b7
An Tran <anpjure@gmail.com>
2024-09-30 19:40:02 +0200

    Remove form tag

--- src/Form.js
+++ src/Form.js
@@ -59,111 +59,111 @@ export default function Form() {
   }
   return (
     <div className={styles.formWrapper}>
-      <form
+      {/*<form
         onSubmit={(event) => {
           event.preventDefault();
         }}
-      >
-        <div className={styles.guestInput}>
-          <div className={styles.inputGroup}>
-            <label htmlFor="firstName">First name: </label>
-            <input
-              value={firstName}
-              id="firstName"
-              onChange={(event) => {
-                setFirstName(event.currentTarget.value);
-              }}
-              disabled={isLoading}
-            />
-          </div>
-          <div className={styles.inputGroup}>
-            <label htmlFor="lastName">Last name: </label>
-            <input
-              value={lastName}
-              id="lastName"
-              disabled={isLoading}
-              onChange={(event) => {
-                setLastName(event.currentTarget.value);
-              }}
-              onKeyDown={async (event) => {
-                if ((event.currentTarget.value && event.key) === 'Enter') {
-                  console.log(guests);
-                  console.log('guests.length' + guests.length);
-                  const tmpGuestId =
-                    guests.length === 0
-                      ? 0
-                      : parseInt(guests[guests.length - 1].id) + 1;
-                  const tmpNewGuest = {
-                    id: tmpGuestId,
-                    firstName: firstName,
-                    lastName: lastName,
-                    attending: false,
-                  };
-                  setGuests([...guests, tmpNewGuest]);
-                  await createNewGuest(firstName, lastName);
-                  setFirstName('');
-                  setLastName('');
-                }
-              }}
-            />
-          </div>
+      > */}
+      <div className={styles.guestInput}>
+        <div className={styles.inputGroup}>
+          <label htmlFor="firstName">First name: </label>
+          <input
+            value={firstName}
+            id="firstName"
+            onChange={(event) => {
+              setFirstName(event.currentTarget.value);
+            }}
+            disabled={isLoading}
+          />
         </div>
+        <div className={styles.inputGroup}>
+          <label htmlFor="lastName">Last name: </label>
+          <input
+            value={lastName}
+            id="lastName"
+            disabled={isLoading}
+            onChange={(event) => {
+              setLastName(event.currentTarget.value);
+            }}
+            onKeyDown={async (event) => {
+              if ((event.currentTarget.value && event.key) === 'Enter') {
+                console.log(guests);
+                console.log('guests.length' + guests.length);
+                const tmpGuestId =
+                  guests.length === 0
+                    ? 0
+                    : parseInt(guests[guests.length - 1].id) + 1;
+                const tmpNewGuest = {
+                  id: tmpGuestId,
+                  firstName: firstName,
+                  lastName: lastName,
+                  attending: false,
+                };
+                setGuests([...guests, tmpNewGuest]);
+                await createNewGuest(firstName, lastName);
+                setFirstName('');
+                setLastName('');
+              }
+            }}
+          />
+        </div>
+      </div>
 
-        <div>
-          {isLoading ? (
-            <div>Loading...</div>
-          ) : (
-            <div>
-              {guests.map((guest, index) => {
-                console.log(`guest: guest`);
-                return (
-                  <div
-                    key={`guest-${guest.id}`}
-                    className={styles.guest}
-                    data-test-id="guest"
-                  >
-                    <input
-                      type="checkbox"
-                      aria-label={
-                        guest.attending
-                          ? `${guest.firstName} ${guest.lastName} true`
-                          : `${guest.firstName} ${guest.lastName} false`
+      <div>
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          <div>
+            {guests.map((guest, index) => {
+              console.log(`guest: guest`);
+              return (
+                <div
+                  key={`guest-${guest.id}`}
+                  className={styles.guest}
+                  data-test-id="guest"
+                >
+                  <input
+                    type="checkbox"
+                    aria-label={
+                      guest.attending
+                        ? `${guest.firstName} ${guest.lastName} true`
+                        : `${guest.firstName} ${guest.lastName} false`
+                    }
+                    checked={guest.attending}
+                    onChange={async () => {
+                      const newGuests = [...guests];
+                      if (newGuests[index].attending) {
+                        newGuests[index].attending = false;
+                      } else {
+                        newGuests[index].attending = true;
                       }
-                      checked={guest.attending}
-                      onChange={async () => {
-                        const newGuests = [...guests];
-                        if (newGuests[index].attending) {
-                          newGuests[index].attending = false;
-                        } else {
-                          newGuests[index].attending = true;
-                        }
-                        setGuests(newGuests);
-                        await updateGuest(
-                          newGuests[index].id,
-                          newGuests[index].attending,
-                        );
-                      }}
-                    />
-                    <div>{guest.firstName}</div>
-                    <div>{guest.lastName}</div>
-                    <button
-                      aria-label={`Remove ${guest.firstName} ${guest.lastName}`}
-                      onClick={async () => {
-                        const newGuests = [...guests];
-                        newGuests.splice(index, 1);
-                        await deleteGuest(guest.id);
-                        setGuests(newGuests);
-                      }}
-                    >
-                      Remove
-                    </button>
-                  </div>
-                );
-              })}
-            </div>
-          )}
-        </div>
-      </form>
+                      setGuests(newGuests);
+                      await updateGuest(
+                        newGuests[index].id,
+                        newGuests[index].attending,
+                      );
+                    }}
+                  />
+                  <div>{guest.firstName}</div>
+                  <div>{guest.lastName}</div>
+                  <button
+                    aria-label={`Remove ${guest.firstName} ${guest.lastName}`}
+                    onClick={async () => {
+                      const newGuests = [...guests];
+                      newGuests.splice(index, 1);
+                      await deleteGuest(guest.id);
+                      setGuests(newGuests);
+                    }}
+                  >
+                    Remove
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
+      {/* </form> */}
     </div>
   );
 }
